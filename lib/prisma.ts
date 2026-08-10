f8e57544de16ff55846d0ff3ef206378f78a73d3
Saeed Mahmoud <saeedroqay@gmail.com>
2026-08-10 10:19:40 +0300

    Connect and seed Prisma Postgres database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -653,7 +653,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: any
 }
 
-const dbUrl = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL || process.env.POSTGRES_URL_NON_POOLING
+const dbUrl = process.env.DATABASE_URL || process.env.PRISMA_DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL || process.env.POSTGRES_URL_NON_POOLING
 
 let prismaInstance: any
 
